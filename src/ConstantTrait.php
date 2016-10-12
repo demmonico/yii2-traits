2b9b92cb7a5a3ab48f336aa7870031dd0d336c27
demmonico <dep.soft@mail.ru>
2016-10-12 13:23:48 +0300

    RF ConstantTrait

--- src/ConstantTrait.php
+++ src/ConstantTrait.php
@@ -1,83 +1,69 @@
 <?php
-/**
- * @author: dep
- * Date: 08.07.16
- */
 
 namespace demmonico\traits;
 
 use demmonico\helpers\ReflectionHelper;
 use Yii;
-use yii\base\Exception;
 
 
 /**
  * Trait generates array of class constant labels or one label by constant value
+ *
+ * @use
+ * cached constant array: to use Yii::$app->cache class-owner of trait should has static property [className::useCacheConstantNames] and set it to true
+ * use custom constant names: class-owner of trait should has static property or method with labels array which will be merged with stock,
+ *                              for example, [className::statusConstantLabels] or [className::statusConstantLabels()] to get labels of STATUS constants
+ *
+ * @author: dep
+ * Date: 08.07.16
  */
 trait ConstantTrait
 {
-    private $useCacheConstantNames = false;
+    public function __call($name, $arguments)
+    {
+        return static::getConstantNames($name, $arguments);
+    }
 
-    // can be set here or in implements class to use custom labels
-    //public $statusLabels = [];
-    //public function $statusLabels(){ return []; };
+    public static function __callStatic($name, $arguments)
+    {
+        return static::getConstantNames($name, $arguments);
+    }
 
-
-
-    public function __call( $name, $arguments )
+    private static function getConstantNames($name, $arguments)
     {
         $class = get_called_class();
         $constPrefix = ReflectionHelper::parseGetterName($name);
 
-        $constArr = ($this->useCacheConstantNames) ? Yii::$app->cache->get($class.$constPrefix) : false;
+        // try to get from cache
+        $isUseCache = property_exists($class, 'useCacheConstantNames') && $class::useCacheConstantNames;
+        $constArr = $isUseCache ? Yii::$app->getCache()->get($class.$constPrefix) : false;
+
+        // else load from class
         if (false === $constArr){
 
             // get const labels
             $constArr = ReflectionHelper::getConstants($class, $constPrefix);
 
             // get overwritten labels
-            $labelsArrName = $constPrefix.'Labels';
-            if (property_exists($class, $labelsArrName) && ($labelsArr = $this->$labelsArrName)
-                || method_exists($class, $labelsArrName) && ($labelsArr = $this->$labelsArrName())
-                AND is_array($labelsArr)
+            $customLabelsName = $constPrefix.'ConstantLabels';
+            if (property_exists($class, $customLabelsName) && ($customLabels = $class::$customLabelsName)
+                || method_exists($class, $customLabelsName) && ($customLabels = $class::$customLabelsName())
+                AND is_array($customLabels)
             ){
-                foreach($labelsArr as $k=>$v) $constArr[$k] = $v;
+                $constArr = array_merge($constArr, $customLabels);
             }
 
             // remember array
-            if ($this->useCacheConstantNames)
+            if ($isUseCache)
                 Yii::$app->cache->set($class.$constPrefix, $constArr);
         }
 
         // return key label or labels array
         if (!empty($constArr) && !empty($arguments) && is_array($arguments)){
             $key = array_shift($arguments);
-            if (isset($constArr[$key]))
-                return $constArr[$key];
-            else
-                throw new Exception('Invalid key');
+            return isset($constArr[$key]) ? $constArr[$key] : '<unknoun>';
         }
-        return $constArr;
-    }
 
-
-
-    public static function __callStatic( $name, $arguments )
-    {
-        $class = get_called_class();
-        $constPrefix = ReflectionHelper::parseGetterName($name);
-
-        // get const labels
-        $constArr = ReflectionHelper::getConstants($class, $constPrefix);
-
-        // return key label or labels array
-        if (!empty($constArr) && !empty($arguments) && is_array($arguments)){
-            $key = array_shift($arguments);
-            if (isset($constArr[$key]))
-                return $constArr[$key];
-            else
-                throw new Exception('Invalid key');
-        }
         return $constArr;
     }
 
